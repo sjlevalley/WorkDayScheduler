967553ac7c707064c15f94bcccc666ff87efbc4c
Steve LeValley <slevalley@gmail.com>
2021-03-25 15:38:10 -0700

    Storage for all time slots now working, just need coloring code

--- script.js
+++ script.js
@@ -38,49 +38,108 @@ saveBtn.on("click", function(event) {
 
 
 function renderSchedule() {
-    
+
+  
+    var storageKey = hour9.attr('id');
+    var value = localStorage.getItem(storageKey);
+    hour9.find(".description").val(value);
+
     var storageKey = hour10.attr('id');
     var value = localStorage.getItem(storageKey);
     hour10.find(".description").val(value);
+
+    var storageKey = hour11.attr('id');
+    var value = localStorage.getItem(storageKey);
+    hour11.find(".description").val(value);
+
+    var storageKey = hour12.attr('id');
+    var value = localStorage.getItem(storageKey);
+    hour12.find(".description").val(value);
+
+    var storageKey = hour13.attr('id');
+    var value = localStorage.getItem(storageKey);
+    hour13.find(".description").val(value);
+
+    var storageKey = hour14.attr('id');
+    var value = localStorage.getItem(storageKey);
+    hour14.find(".description").val(value);
+
+    var storageKey = hour15.attr('id');
+    var value = localStorage.getItem(storageKey);
+    hour15.find(".description").val(value);
+
+    var storageKey = hour16.attr('id');
+    var value = localStorage.getItem(storageKey);
+    hour16.find(".description").val(value);
+
+    var storageKey = hour17.attr('id');
+    var value = localStorage.getItem(storageKey);
+    hour17.find(".description").val(value);
+
     
 };
 renderSchedule();
 
 
 
+// ###################### Hour Coloring ####################
 
+// function colorShift() {
+//     var currentHour = moment().hour();
+//     console.log(currentHour);
 
-
-
-
-
-// ################ Reference code ###################
-
-
-// var projectForm = $('#projectForm');
-// projectForm.on("submit",addItem);
-// projectList=[];
-// function addItem(event) {
-//     event.preventDefault();
-//     projectItem = {
-//         name: $("#nameInput").val(),
-//         type: $("#typeInput").val(),
-//         rate: $("#rateInput").val(),
-//         date: $("#datepicker").val()
-//     };
-//     projectList.push(projectItem);
-//     $("#nameInput").val("");
-//     $("#typeInput").val("");
-//     $("#rateInput").val("");
-//     $("#datepicker").val("");
+//     if(currentHour <= 8 || currentHour >=17)
 // }
 
 
 
-// ##################### REFERENCE CODE #################
-// if (window.localStorage["taskText"]) {
-//     txt1.value = window.localStorage["taskText"];
-// }
-// txt1.addEventListener("keyup", function () {
-//     window.localStorage["taskText"] = txt1.value;
-// })
+
+
+
+
+
+
+
+
+
+
+    
+// function renderSchedule() {
+    
+//     var storageKey = hour10.attr('id');
+//     var value = localStorage.getItem(storageKey);
+//     hour10.find(".description").val(value);
+    
+// };
+// renderSchedule();
+
+    // var storageKey9 = hour9.attr('id');
+    // var value9 = localStorage.getItem(storageKey9);
+    // hour9.find(".description").val(value9);
+    
+    // var storageKey10 = hour10.attr('id');
+    // var value10 = localStorage.getItem(storageKey10);
+    // hour10.find(".description").val(value10);
+
+    // var storageKey11 = hour11.attr('id');
+    // var value11 = localStorage.getItem(storageKey11);
+    // hour11.find(".description").val(value11);
+    
+    // var storageKey12 = hour12.attr('id');
+    // var value12 = localStorage.getItem(storageKey12);
+    // hour12.find(".description").val(value12);
+
+    // var storageKey = hour10.attr('id');
+    // var value = localStorage.getItem(storageKey);
+    // hour10.find(".description").val(value);
+    
+
+
+
+
+
+
+
+
+
+
